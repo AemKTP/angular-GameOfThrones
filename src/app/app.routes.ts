e78594d945e9a5ba05c459192ef90bad1e7c6079
AemKTP <kittipatthongkhun@gmail.com>
2024-01-28 09:54:10 +0700

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './page/login/login.component';
+import { MainComponent } from './page/main/main.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {path: '' , component:MainComponent},
+    {path: 'login' , component:LoginComponent}
+];
