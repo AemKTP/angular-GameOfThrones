705b996d65f14b2bf978c3c4a495830655ae48b3
AemKTP <kittipatthongkhun@gmail.com>
2024-02-02 23:40:12 +0700

    Finish

--- src/app/page/login/login.component.ts
+++ src/app/page/login/login.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Router } from 'express';
+import { routes } from '../../app.routes';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
--- src/app/page/main/main.component.ts
+++ src/app/page/main/main.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCommonModule } from '@angular/material/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatRippleModule } from '@angular/material/core';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
+import { Router } from 'express';
+import { routes } from '../../app.routes';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [MatToolbarModule],
+  imports: [MatToolbarModule, MatFormFieldModule, MatCommonModule, MatInputModule, MatIconModule, MatRippleModule, MatButtonModule, RouterLink],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
